bd9d1cc6f798c3f0fbe35e83313fd1dec86b2cc3
Amir <hesamian@uwm.edu>
2024-07-20 10:27:46 -0500

    updated footnote

--- ui/src/components/footer/index.jsx
+++ ui/src/components/footer/index.jsx
@@ -1,6 +1,6 @@
 const Footer = () => <>
   <footer className="pt-2 mt-4 text-muted border-top d-flex justify-content-center">
-      &copy; December 2023
+      &copy; December 2023 - Milwaukee, WI
   </footer>
 </>;
 
