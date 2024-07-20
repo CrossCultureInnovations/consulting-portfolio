e9850bcb7a79ef4789c0727d012e532a44f1e901
Amir <hesamian@uwm.edu>
2024-07-19 21:29:05 -0500

    update name

--- ui/src/components/jumbotron/index.jsx
+++ ui/src/components/jumbotron/index.jsx
@@ -7,7 +7,7 @@ const Jumbotron = () =>
       <Row>
         <Col sm={12} md={8}>
           <h1 className="display-5 fw-bold">
-            Cross Cultural Invocation Consulting Team
+            Cross Cultural Invocation Consulting
           </h1>
           <p className="col-md-8 fs-4">
             Your local software solution experts, providing innovative services to propel your business forward. Contact
