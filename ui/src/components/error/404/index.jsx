b78b1b9bbc4fe67c1b4c93051cb019f8f3848ebd
Amir <hesamian@uwm.edu>
2024-07-20 20:58:51 -0500

    added not found page

--- ui/src/components/error/404/index.jsx
+++ ui/src/components/error/404/index.jsx
@@ -1,3 +1,14 @@
-const Error404 = () => <p>Page Not Found</p>;
+import { Container } from "react-bootstrap";
+
+const Error404 = () =>
+  <Container fluid>
+    <div style={{
+      maxWidth: "fit-content",
+      margin: "auto"
+    }}>
+      <p>Page Not Found</p>
+      <img src="https://http.cat/images/404.jpg" className="img-fluid" />
+    </div>
+  </Container>;
 
 export default Error404;
